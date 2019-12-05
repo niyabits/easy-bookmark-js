2668b0d3363908def62dc04ba5c9a8305f3513cd
Yash <yashguptaofficial@gmail.com>
2019-12-05 13:29:47 +0530

    Added More stuff

--- app.js
+++ app.js
@@ -3,34 +3,68 @@ const form = document.querySelector("#app-form");
 const bookmark = document.querySelector("#bookmark");
 const appContainer = document.querySelector(".app-container");
 
-const bookmarksArr = [];
+let bookmarksArr = [];
 
 // Make markup function
 const makeMarkup = item => {
   const itemMarkup = `
     <div class="bookmarked-url">
       <a href="${item}" target="_blank">${item}</a>
-      <a href="#" class="delete-item">
-        <span>x</span>
+      <a href="#">
+        <span class="delete-item">x</span>
       </a>
     </div>
   `;
   return itemMarkup;
 };
 
+// Fill Local Storage with the Array
+const setLS = () => {
+  const bookmarkArrStr = JSON.stringify(bookmarksArr)
+  localStorage.setItem("state", bookmarkArrStr);
+}
+
+// Load all the bookmarks when the app starts
+window.addEventListener('DOMContentLoaded', _ => { 
+  let bookmarksJSON = localStorage.getItem("state");
+  bookmarksArr = JSON.parse(bookmarksJSON);
+  
+  bookmarksArr.forEach(item => {
+    appContainer.innerHTML += makeMarkup(item)
+  })
+})
+
 // Adding event listerner to the form
 form.addEventListener("submit", e => {
   e.preventDefault();
 
-  // Add item to array
-  bookmarksArr.push(bookmark.value);
+  const re = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
 
-  // Add item to UI
-  appContainer.innerHTML += makeMarkup(bookmark.value);
+  if (bookmark.value.match(re)) {
+    // Add item to array
+    bookmarksArr.push(bookmark.value);
+  
+    // Add item to UI
+    appContainer.innerHTML += makeMarkup(bookmark.value);
+  
+    // Clear after submit
+    bookmark.value = "";
+  
+    setLS()
+  } else {
+    alert("Something is wrong with your URL")
+  }
 
-  // Clear after submit
-  bookmark.value = "";
-
-  // Test
-  console.log(bookmarksArr);
 });
+
+appContainer.addEventListener('click', e => {
+  e.preventDefault()
+  
+  if(e.target.className === 'delete-item') {
+    curItem =  e.target.parentElement.parentElement.children[0].innerHTML
+    bookmarksArr = bookmarksArr.filter(item => item !== curItem)
+    
+    setLS()
+    e.target.parentElement.parentElement.remove()
+  }
+})
